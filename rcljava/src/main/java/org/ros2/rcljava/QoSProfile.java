bbe8d3a4a7930888f0fe199da1370c92f0e49cfa
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2016-08-29 02:28:14 +0200

    Add QOS profile

--- rcljava/src/main/java/org/ros2/rcljava/QoSProfile.java
+++ rcljava/src/main/java/org/ros2/rcljava/QoSProfile.java
@@ -15,16 +15,113 @@
 package org.ros2.rcljava;
 
 /**
- * <h1>Quality of Service</h1>
+ * <h1>Quality of Service Profile</h1>
  * <p><i>TODO Not full implemented !!</i></p>
  * @author Mickael Gaillard <mickael.gaillard@gmail.com>
  */
 public class QoSProfile {
 
-    public static final QoSProfile DEFAULT = new QoSProfile();
+    /** Default Depth level */
+    public static final int DEPTH_SYSTEM_DEFAULT = 0;
 
-    public QoSProfile() {
-
+    /** Reliability Policy enum. */
+    public enum ReliabilityPolicy {
+        SYSTEM_DEFAULT,
+        RELIABLE,
+        BEST_EFFORT
     }
 
+    /** History Policy enum. */
+    public enum HistoryPolicy {
+        SYSTEM_DEFAULT,
+        KEEP_LAST,
+        KEEP_ALL
+    }
+
+    /** Durability Policy enum. */
+    public enum DurabilityPolicy {
+        SYSTEM_DEFAULT,
+        TRANSIENT_LOCAL,
+        VOLATILE
+    }
+
+    /** Sensor QOS Profile */
+    public static final QoSProfile PROFILE_SENSOR_DATA = new QoSProfile(
+            HistoryPolicy.KEEP_LAST,
+            5,
+            ReliabilityPolicy.BEST_EFFORT,
+            DurabilityPolicy.SYSTEM_DEFAULT
+            );
+
+    /** Parameter QOS Profile */
+    public static final QoSProfile PROFILE_PARAMETER = new QoSProfile(
+            HistoryPolicy.KEEP_LAST,
+            1000,
+            ReliabilityPolicy.RELIABLE,
+            DurabilityPolicy.SYSTEM_DEFAULT
+            );
+
+    /** Default QOS Profile */
+    public static final QoSProfile PROFILE_DEFAULT = new QoSProfile(
+            HistoryPolicy.KEEP_ALL,
+            10,
+            ReliabilityPolicy.RELIABLE,
+            DurabilityPolicy.SYSTEM_DEFAULT
+            );
+
+    /** Services default QOS Profile */
+    public static final QoSProfile PROFILE_SERVICES_DEFAULT = new QoSProfile(
+            HistoryPolicy.KEEP_LAST,
+            10,
+            ReliabilityPolicy.RELIABLE,
+            DurabilityPolicy.TRANSIENT_LOCAL
+            );
+
+    /** Parameter events QOS Profile */
+    public static final QoSProfile PROFILE_PARAMETER_EVENTS = new QoSProfile(
+            HistoryPolicy.KEEP_ALL,
+            1000,
+            ReliabilityPolicy.RELIABLE,
+            DurabilityPolicy.SYSTEM_DEFAULT
+            );
+
+    /** System default QOS Profile */
+    public static final QoSProfile PROFILE_SYSTEM_DEFAULT = new QoSProfile(
+            HistoryPolicy.SYSTEM_DEFAULT,
+            DEPTH_SYSTEM_DEFAULT,
+            ReliabilityPolicy.SYSTEM_DEFAULT,
+            DurabilityPolicy.SYSTEM_DEFAULT
+            );
+
+    /** History Policy */
+    protected final HistoryPolicy history;
+
+    /** Depth */
+    protected final int depth;
+
+    /** Reliability Policy */
+    protected final ReliabilityPolicy reliability;
+
+    /** Durability Policy */
+    protected final DurabilityPolicy durability;
+
+    /**
+     * Constuctor.
+     *
+     * @param history
+     * @param depth
+     * @param reliability
+     * @param durability
+     */
+    public QoSProfile(
+            HistoryPolicy history,
+            int depth,
+            ReliabilityPolicy reliability,
+            DurabilityPolicy durability) {
+
+        this.history = history;
+        this.depth = depth;
+        this.reliability = reliability;
+        this.durability = durability;
+    }
 }
