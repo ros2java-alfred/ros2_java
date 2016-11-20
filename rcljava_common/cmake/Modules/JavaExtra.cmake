09949d545445a81009bf56d4baa6a4506f34a28a
Esteve Fernandez <esteve@apache.org>
2016-11-20 15:19:15 +0100

    Convert JavaExtra into a plain CMake module.

--- rcljava_common/cmake/Modules/JavaExtra.cmake
+++ rcljava_common/cmake/Modules/JavaExtra.cmake
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include(CrossCompilingExtra)
+
 if(ANDROID)
   find_host_package(Java COMPONENTS Development)
 else()
