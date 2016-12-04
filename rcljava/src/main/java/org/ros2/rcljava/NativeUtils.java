3a6fe6abd0ee7d133941243507a31b94427f5752
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2016-12-04 04:12:39 +0100

    Restore bad deleted file

---
+++ rcljava/src/main/java/org/ros2/rcljava/NativeUtils.java
@@ -0,0 +1,52 @@
+/* Copyright 2016 Esteve Fernandez <esteve@apache.org>
+ * Copyright 2016 Mickael Gaillard <mick.gaillard@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ros2.rcljava;
+
+import java.util.Vector;
+
+/**
+ * Utility for load JNI.
+ */
+public abstract class NativeUtils {
+
+    private static java.lang.reflect.Field LIBRARIES;
+
+    static {
+        try {
+            LIBRARIES = ClassLoader.class.getDeclaredField("loadedLibraryNames");
+            LIBRARIES.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static String[] getLoadedLibraries(final ClassLoader loader) {
+        Vector<String> libraries = new Vector<String>();
+
+        try {
+            libraries = (Vector<String>) LIBRARIES.get(loader);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return libraries.toArray(new String[] {});
+    }
+}
