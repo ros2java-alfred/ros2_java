d9b9e4d91d2b22dffefa0c9c15c9107d058e3910
Erwan Le Huitouze <erwan.lehuitouze@tactfactory.com>
2017-06-01 22:04:06 +0200

    Fixes #9 Change std to c++14 in generate interface java

--- rosidl_generator_java/cmake/rosidl_generator_java_generate_interfaces.cmake
+++ rosidl_generator_java/cmake/rosidl_generator_java_generate_interfaces.cmake
@@ -29,7 +29,7 @@ endif()
 include(UseJava)
 
 if(NOT WIN32)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -Wall -Wextra")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 -Wall -Wextra")
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
 endif()
 
